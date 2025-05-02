35ee8660f2b07125a74fe4f0c45bd5d16141f47d
SofyaGrischenko <sofya10293847@gmail.com>
2025-05-02 11:01:59 +0300

    feat: add error handlers

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,11 +1,13 @@
 import '../styles/index.scss';
 
-const keys = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0'];
+const keys = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.'];
 const operators = ['c', '+', '–', '/', 'x', '%', '+/-', '='];
+
 let operatorsFlag = false;
-let currentOperator = '';
+let currentOperator = ' ';
 let firstOperand = 0;
 let secondOperand = 0;
+let answerFlag = false;
 
 export function createKeyboard() {
   const keyboard = document.createElement('div');
@@ -14,18 +16,16 @@ export function createKeyboard() {
   const numsContainer = document.createElement('div');
   numsContainer.classList.add('numpad');
   for (let key of keys) {
-    const button = buttons(key);
-    button.addEventListener('click', handleNums(key));
-
+    const button = createButton(key);
+    button.addEventListener('click', handleNumsClick(key));
     numsContainer.appendChild(button);
   }
 
   const operatorsContainer = document.createElement('div');
   operatorsContainer.classList.add('operators');
   for (let key of operators) {
-    const button = buttons(key);
-    button.addEventListener('click', handleOperator(key));
-
+    const button = createButton(key);
+    button.addEventListener('click', handleOperatorClick(key));
     operatorsContainer.appendChild(button);
   }
 
@@ -33,65 +33,89 @@ export function createKeyboard() {
   return keyboard;
 }
 
-function buttons(text) {
+function createButton(text) {
   const button = document.createElement('button');
   button.textContent = text;
   button.classList.add('button');
-
   return button;
 }
 
-function handleNums(key) {
+function handleNumsClick(key) {
   return () => {
     const input = document.querySelector('.input');
-    input.value += key;
+    if (input.value === '0' || input.value === 'Error') {
+      input.value = key;
+    } else {
+      input.value += key;
+    }
   };
 }
 
-function handleOperator(key) {
+function handleOperatorClick(key) {
   return () => {
     const input = document.querySelector('.input');
 
     if (key === 'c') {
-      input.value = input.value.slice(0, -1);
+      if (answerFlag || input.value.length === 1) {
+        input.value = '0';
+        currentOperator = '';
+        operatorsFlag = false;
+        answerFlag = false;
+      } else {
+        input.value = input.value.slice(0, -1);
+        if (!input.value.includes(currentOperator)) {
+          operatorsFlag = false;
+          currentOperator = '';
+        }
+      }
       return;
     }
 
-    if (!operatorsFlag) {
-      currentOperator = key;
-      firstOperand = parseInt(input.value);
-      input.value = '';
-      operatorsFlag = true;
+    if (key === '+/-') {
+      input.value = String(parseFloat(input.value) * -1);
+      return;
     }
 
-    if (key === '=') {
-      input.value
-        ? (secondOperand = parseInt(input.value))
-        : (secondOperand = 0);
+    if (key === '=' && operatorsFlag && currentOperator !== ' ') {
+      const [first, second] = input.value.split(currentOperator);
+      if (!second || isNaN(parseFloat(second))) {
+        return;
+      }
+      firstOperand = parseFloat(first);
+      secondOperand = parseFloat(second);
 
+      let answer;
       switch (currentOperator) {
         case '+':
-          input.value = firstOperand + secondOperand;
+          answer = firstOperand + secondOperand;
           break;
         case '–':
-          input.value = firstOperand - secondOperand;
+          answer = firstOperand - secondOperand;
           break;
         case '/':
-          input.value = firstOperand / secondOperand;
+          answer = secondOperand === 0 ? 'Error' : firstOperand / secondOperand;
           break;
         case 'x':
-          input.value = firstOperand * secondOperand;
+          answer = firstOperand * secondOperand;
           break;
         case '%':
-          input.value = (firstOperand / 100) * secondOperand;
-          break;
-        case '+/-':
-          input.value = -firstOperand;
+          answer = firstOperand * (secondOperand / 100);
           break;
       }
+
+      input.value = answer.toString();
       operatorsFlag = false;
-      currentOperator = '';
+      currentOperator = ' ';
       firstOperand = 0;
+      secondOperand = 0;
+      answerFlag = true;
+      return;
+    }
+
+    if (!operatorsFlag && key !== '=' && key !== 'c') {
+      currentOperator = key;
+      input.value += key;
+      operatorsFlag = true;
     }
   };
 }
